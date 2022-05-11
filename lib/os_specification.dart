3971bf6ce8d624e3b851c8bfff28dadd3ea48aa9
alex <a99som@yandex.ru>
2022-05-11 13:14:05 +0300

    fix external library

--- lib/os_specification.dart
+++ lib/os_specification.dart
@@ -6,6 +6,6 @@ library os_specification;
 export 'package:os_specification/src/os/linux.dart' show Linux;
 export 'package:os_specification/src/os/os_spec.dart' show OsSpecifications;
 export 'package:os_specification/src/os/windows.dart' show Windows;
-export 'package:win_api/src/win_api.dart' show WinEnvironment;
+export 'package:win_api/src/win_api.dart' show WinEnvironment, ShowMode;
 
 
